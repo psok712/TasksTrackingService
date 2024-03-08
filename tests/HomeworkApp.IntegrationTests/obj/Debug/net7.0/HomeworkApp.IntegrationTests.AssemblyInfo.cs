ca2a6de9d7c4442eabd56c630973bb635c812d08
mgrishkov <mgrishkov@ozon.ru>
2024-03-08 15:10:07 +0300

    * add gitignore

--- tests/HomeworkApp.IntegrationTests/obj/Debug/net7.0/HomeworkApp.IntegrationTests.AssemblyInfo.cs
+++ tests/HomeworkApp.IntegrationTests/obj/Debug/net7.0/HomeworkApp.IntegrationTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeworkApp.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f01580e68beb91e4e767b33c5c0e6e3ebc695aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27db7915943236133c49d9d6984ce4cabf559f7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeworkApp.IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeworkApp.IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
